0e0aa4d1cbcfb78c615b3c767136c4d200a12e14
tommydebisi <tommydebisi@gmail.com>
2023-02-01 04:23:53 +0100

    :seedling: reverted changes

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -16,10 +16,13 @@ class Config:
 app.config.from_object(Config)
 babel = Babel(app)
 
-@babel.localselector
+
 def get_locale() -> str:
     """ get the translation best match """
     return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/', strict_slashes=False)
